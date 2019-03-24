84bd7f363347ae50fc9e544f3820903b2ad57ef6
Jak <jak@jakgrueneberg.com>
2019-03-24 11:09:37 -0700

    Fix history and LINK
    faster routing, and avoid page refresh with LINK.
    Consolidated Route.
    Added history to Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import './App.scss';
 import './fonts/fa-all-min.css';
 import { connect } from 'react-redux';
+import { history } from './js/store/index';
 import Header from './js/components/Header';
 import DarkModeToggle from './js/components/DarkModeToggle';
 import Footer from './js/components/Footer';
@@ -16,9 +17,10 @@ import axios from 'axios';
 import staticData from './js/constants/static-data';
 import {parseStoryblockData} from './js/helpers/helpers';
 import {APP_LOADED} from './js/constants/action-types';
-import {
-  BrowserRouter as Router,
-  Route,
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Link, 
   Switch
 } from 'react-router-dom';
 
@@ -40,23 +42,25 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Header/>
-        <DarkModeToggle/>
-        <div className="main">
-          <Router>
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/news" exact component={News} />
-              <Route path="/about" exact component={About} />
-              <Route path="/online-research-help" exact component={OnlineResearchHelp} />
-              <Route path="/articles" exact component={Articles} />
-              <Route path="/articles/:page" component={Articles} />
-              <Route path="/articledetail/" component={ArticleDetail} />
-              <Route component={NoMatch} />
-            </Switch>
-          </Router>
-        </div>
-        <Footer/>
+        <Router history={history}>
+          <div>
+            <Header/>
+            <DarkModeToggle/>
+            <div className="main">
+              <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/news" exact component={News} />
+                <Route path="/about" exact component={About} />
+                <Route path="/online-research-help" exact component={OnlineResearchHelp} />
+                <Route path="/articles" exact component={Articles} />
+                <Route path="/articles/:page" component={Articles} />
+                <Route path="/articledetail/" component={ArticleDetail} />
+                <Route component={NoMatch} />
+              </Switch>
+            </div>
+          <Footer/>
+          </div>
+        </Router>
       </div>
     );
   }
